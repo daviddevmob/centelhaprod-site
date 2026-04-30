28f452aee7043180ef8ebc5d65f65bf3b6a0227f
daviddevmob <60259424+daviddevmob@users.noreply.github.com>
2026-04-30 11:26:48 -0300

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -6,7 +6,7 @@ export const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-03-01', // use a UTC date string
   useCdn: false, // set to `false` to bypass the edge cache
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  token: process.env.SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
